00df26f6b5ac02f7620569768783ba4d7a896d3a
Grigor_Ina <grigorina72@gmail.com>
2024-02-04 12:53:35 +0200

    Add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[i];
+while (i < n)
+{
+    if (max < array[i])
+    { 
+        max = array[i];
+        }
+    i = i + 1;
+}
+
+Console.WriteLine(max);
