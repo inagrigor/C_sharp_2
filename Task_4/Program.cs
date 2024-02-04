9d42de169a62b37c00ff4ac98d43c29fa385708a
Grigor_Ina <grigorina72@gmail.com>
2024-02-04 13:48:10 +0200

    ciclu for foreach while

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,14 +1,37 @@
 ﻿int n = 5;
-int[] array = { 2, 4, 7, 3, 9 };
+int[] array = { 12, 40, 17, 30, 19 };
 int i = 0;
 int max = array[i];
 while (i < n)
 {
     if (max < array[i])
-    { 
+    {
         max = array[i];
-        }
+    }
     i = i + 1;
 }
 
 Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (max < array[j])
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+
+    if (max < e)
+    {
+        max = e;
+    }
+
+Console.WriteLine(max);
+
+
+
